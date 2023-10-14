124f3490c321f45b8e359bacef2a3c4fb0b51b84
haalwahedi <82106424+haalwahedi@users.noreply.github.com>
2023-10-14 19:37:21 +0400

    Delete backend directory

--- backend/src/auth/Guards.ts
+++
@@ -1,8 +0,0 @@
-import { CanActivate, ExecutionContext, Injectable } from "@nestjs/common";
-import { AuthGuard } from "@nestjs/passport";
-
-@Injectable()
-export class authGuard extends AuthGuard('42'){}
-
-@Injectable()
-export class jwtGuard extends AuthGuard('jwt'){}
